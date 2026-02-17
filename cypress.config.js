35de19032b5594834fb96b4aa4fbf01434bd9d3e
Vitor Campesatto <vitoorcamp@hotmail.com>
2026-02-16 23:34:46 -0500

    Configure Mochawesome reporting and ignore generated reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
+  video: true,
+  screenshotOnRunFailure: true,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
+
